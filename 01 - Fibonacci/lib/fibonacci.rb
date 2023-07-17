484dc4279de5eabca8119b2f399304a07ef5a48b
The-Flying-Dev <leandranm@outlook.com>
2023-07-17 18:51:58 +0200

    Fibonnaci

--- 01 - Fibonacci/lib/fibonacci.rb
+++ 01 - Fibonacci/lib/fibonacci.rb
@@ -1,5 +1,13 @@
 class Fibonacci
   def self.calculate(n)
     # View README.md for instructions
+    n = n + 1
+    first, second = [0, 1]
+
+    (n - 1).times do 
+      first, second = second, first + second 
+    end 
+
+    return first 
   end
 end
